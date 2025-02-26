bd75028b947bfebe49c5a0a15fbcec71731fe43c
arefshal <arefshal@yahoo.com>
2025-02-26 07:26:25 +0330

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Aref Shalchi on 2/26/25.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
